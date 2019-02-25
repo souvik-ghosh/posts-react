81fa52c364538c9a5d35e15d1c5b8f614c8ad6ad
Souvik <emailtosvk@gmail.com>
2019-02-26 03:27:18 +0530

    stage 01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import Posts from './components/Posts';
+import reducer from './reducer';
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default function App() {
   return (
-    <div className="App">
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Posts />
+      </div>
+    </Provider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import reducer from './reducer';
 
-const store = createStore(reducer, applyMiddleware(thunk));
-
-const ReduxApp = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-
-ReactDOM.render(<ReduxApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
